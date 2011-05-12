5dc23d75290022987e9b681ccdb372a338acf05f
Scott Bulua <scott@Scott-Buluas-MacBook-Pro.local>
2011-05-12 16:11:06 -0700

    rails new opentrak

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opentrak::Application.config.secret_token = '6cfcff9cadb71126b62ab5f78263c40e2d58e5b9aabb08655c463e79cdc878ec1f9d019e3d361c9463982dbb0b025f7a7f89fb661176d8ac10d37fe23d02eb24'
