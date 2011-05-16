8180da97ca4b278b15078132b2b85fcd9cf8bf3f
Scott Bulua <scott@Scott-Buluas-MacBook-Pro.local>
2011-05-16 14:35:20 -0700

    simplify migration

--- db/migrate/20110516211824_add_count_to_open.rb
+++ db/migrate/20110516211824_add_count_to_open.rb
@@ -1,10 +1,6 @@
 class AddCountToOpen < ActiveRecord::Migration
   def self.up
     add_column :opens, :count, :integer, :default => 1
-    
-    Open.all.each do |x|
-      x.update_attribute(:count, 1)
-    end
   end
 
   def self.down
