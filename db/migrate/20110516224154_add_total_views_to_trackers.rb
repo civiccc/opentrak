4f7b539ac53f739b0ea8bc37f327be784702b8e5
Scott Bulua <scott@Scott-Buluas-MacBook-Pro.local>
2011-05-16 15:46:22 -0700

    heroku does not like to do work in migrations

--- db/migrate/20110516224154_add_total_views_to_trackers.rb
+++ db/migrate/20110516224154_add_total_views_to_trackers.rb
@@ -1,10 +1,6 @@
 class AddTotalViewsToTrackers < ActiveRecord::Migration
   def self.up
     add_column :trackers, :total_views, :integer, :default => 0
-    
-    Tracker.all.each do |x|
-      x.update_attribute(:total_views, x.calc_total_views)
-    end
   end
 
   def self.down
